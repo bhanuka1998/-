2c27a611675ef5d66280f3e6594f8f4319cc7a10
bhanuka1998 <bhanuka06@gmail.com>
2024-12-29 09:59:32 +0530

    Minor changes in the code

---
+++ src/util/validationSchemas.js
@@ -0,0 +1,14 @@
+// validationSchemas.js
+import * as Yup from 'yup';
+
+export const studentValidationSchema = Yup.object().shape({
+  studentId: Yup.string()
+    .required('Student ID is required')
+    .matches(/^\d+$/, 'Student ID must only contain numbers'),
+  studentName: Yup.string()
+    .required('Student Name is required')
+    .matches(/^[A-Za-z\s]+$/, 'Student Name must only contain letters'),
+  studentEmail: Yup.string()
+    .required('Student Email is required')
+    .email('Enter a valid email address'),
+});
